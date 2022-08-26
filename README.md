cdfc00512ca20053e74a6cd4042a7e416c4d9d38
Dallas Hyde <39468324+hydedall@users.noreply.github.com>
2022-08-26 02:26:54 -0400

    Project Final

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
-///////////////////////////////////////
-////// I DONT KNOW WHAT ANIME TO WATCH
-///////////////////////////////////////
+# I Don't Know What Anime To Watch
 
-///////////////////////////////////////
-////// Technology Used
-///////////////////////////////////////
+Utilizes the Jikan (non-official myanimelist.com) API.
 
-- HTTP
-- CSS
-- JavaScript/Jquery
-- Bootstrap
-- Jikan API (Unofficial MyAnimeList.com API)
+## Description
 
-////////////////////////////////////////////
-///////
+Using the Jikan API to pull a random anime from the myanimelist.com database. All the user needs to do is hit the random button, and the API will return a show. The site also tells the user the year a show released, as well as the rating and review score from myanimelist.com.
+
+## Technologies Used
+
+* HTML 5
+* CSS3
+* JavaScript
+* jQuery
+* Google Fonts
+* Jikan API
+
+### Screenshots
+
+![App Screenshot](https://cdn.discordapp.com/attachments/723717749834121259/1012605605573316678/Screenshot_from_2022-08-26_02-08-20.png)
+![App Screenshot](https://cdn.discordapp.com/attachments/723717749834121259/1012605605938216980/Screenshot_from_2022-08-26_02-11-02.png)
 
 
+### Getting Started
+
+[Click here](hydedall.github.io)
+
+## Authors
+
+ex. Dallas Hyde 
+
+---
--- script.js
+++ script.js
@@ -2,9 +2,6 @@ $("#random_button").click(function () {
     $.get("https://api.jikan.moe/v4/random/anime?sfw", function (data, status) {
         let image = 'https://pbs.twimg.com/profile_images/1190380284295950339/Py6XnxvH_400x400.jpg';
         // console.log(data);
-        // const trailer = data.data.trailer;
-        // $("div.video_holder")
-        //     .html(`<iframe width="560" height="315" src=${data.data.trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
         const title = data.data.title;
         $('div.title_holder')
             .text(title)
@@ -30,8 +27,15 @@ $("#random_button").click(function () {
                 .text('')
         }
         const year = data.data.year;
-        $('div.year_holder')
-            .text(year)
+        if (year != undefined) {
+            $('#year_title').show();
+            $('div.year_holder')
+                .text(year)
+        } else {
+            $('#score_title').hide();
+            $('div.score_holder')
+                .text('')
+        }
 
         image = data.data.images.jpg.large_image_url;
         if (rating != 'Rx - Hentai' || rating != 'R+ - Mild Nudity') {
